025cdb5052f799cecc92d4ef9908d802b1f1b30c
Luiz <luiz.veloso0199@gmail.com>
2019-05-21 19:42:32 -0300

    Update prototype package

--- JDesignPatterns/src/dp/creational/Prototype/Main.java
+++ JDesignPatterns/src/dp/creational/Prototype/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Prototype;
+package dp.Creational.Prototype;
 
 import java.util.ArrayList;
 import java.util.List;
