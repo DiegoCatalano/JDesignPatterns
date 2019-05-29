532a40406b4dd0d27c3904a8dfde7379a0c5a7a7
Mateus <mateusmfm12@gmail.com>
2019-05-29 13:56:21 -0300

    visitor

---
+++ JDesignPatterns/src/dp/behavioral/Visitor/Elemento.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp.behavioral.Visitor;
+
+/**
+ *
+ * @author mateu
+ */
+public class Elemento {
+    public void accept (Visitor visitor){        
+    }
+}
